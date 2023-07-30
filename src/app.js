b6bf9d79d5777d8361dc41070e79fc08d19195d1
JH CHO <junh9541@gmail.com>
2023-07-30 23:27:03 +0900

    feat: 프론트 연동, prettier 설정파일 추가
    express 포트 5000으로 변경
    CORS 설정: CORS 미들웨어 사용. 프론트 백엔드 통신 가능
    빌드폴더 경로 추가

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
 import 'dotenv/config';
 import express from 'express';
-import db from './db';
+import path from 'path';
+import db from './db.js';
+import cors from 'cors';
 
 import productRouters from './routers/productRouters';
 
+const __dirname = path.resolve();
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
-app.get('/', (req, res) => {
-  res.send('hello');
-});
+//프론트 통신을 위한 CORS 설정
+app.use(cors({ origin: 'http://localhost:3000' }));
+
+app.use(express.static(path.join(__dirname, '../front-end/build')));
 
 app.use('/product', productRouters);
+
+app.get('*', (req, res) => {
+  //나머지 경로로 요청이 올 시 front의 빌드 파일 반환
+  res.sendFile(path.join(__dirname, '../front-end/build/index.html'));
+});
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기 중');
