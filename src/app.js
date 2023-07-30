06fa7e9ee4a6b7428a2ee4ca801d92d87e32eca3
KimJiyun <magi83105@gmail.com>
2023-07-30 23:24:46 +0900

    chore: CORE해결, react 연결 확인

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
-import 'dotenv/config';
-import express from 'express';
-import db from './db';
+import express from "express";
+import "dotenv/config";
+import db from "./db";
 
-import productRouters from './routers/productRouters';
+import productRouters from "./routers/productRouters";
+const test = require("./routers/login");
 
 const app = express();
+const cors = require("cors");
 
-app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('hello');
+app.use(cors());
+app.set("port", 5000);
+
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
-app.use('/product', productRouters);
+app.use("/product", productRouters);
+app.use("/login", test);
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기 중');
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
 });
