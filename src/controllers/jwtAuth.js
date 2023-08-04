99d9b0124c7e90a2142c57d0fe70d1f5b3712b63
KimJiyun <magi83105@gmail.com>
2023-08-04 15:54:02 +0900

    refactor: user관련 코드 수정
    1. CookieJwtAuth -> jwtAuth
    2. userController로 분리

--- src/controllers/jwtAuth.js
+++ src/controllers/jwtAuth.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-export const cookieJwtAuth = (req, res, next) => {
+export const jwtAuth = (req, res, next) => {
   const token = req.cookies.jwt;
   try {
     req.user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,40 +2,12 @@ import express from 'express';
 import db from '../db';
 import jwt from 'jsonwebtoken';
 
-import { cookieJwtAuth } from '../controllers/jwtAuth';
+import { jwtAuth } from '../controllers/jwtAuth';
+import { postLogin, getLogout } from '../controllers/userController';
 
 const userRouter = express.Router();
 
-userRouter.post('/login', (req, res) => {
-  const { id, password } = req.body;
-  db.query(
-    `select * from user where userID='${id}' and password='${password}'`,
-    (err, results) => {
-      try {
-        if (results.length === 1) {
-          const token = jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, {
-            expiresIn: '30m',
-          });
-          res.cookie('jwt', token);
-          return res.send({ login: true });
-        } else {
-          return res.send({ login: false });
-        }
-      } catch (err) {
-        return res.send({ login: false });
-      }
-    },
-  );
-});
-
-userRouter.post('/logout', cookieJwtAuth, (req, res) => {
-  if (!req.user) {
-    console.log('로그인 상태가 아닙니다.');
-  } else {
-    res.clearCookie('jwt');
-    console.log('로그아웃.');
-  }
-  res.end();
-});
+userRouter.post('/login', postLogin);
+userRouter.get('/logout', jwtAuth, getLogout);
 
 export default userRouter;
